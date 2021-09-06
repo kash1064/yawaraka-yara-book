efdf1434d7c6313fa26206f7bb517ccb9fd53611
kash1064 <kaship24@gmail.com>
2021-09-06 23:48:51 +0900

    add callback script

---
+++ yaramaster/python_scripts/08_callback_sample2.py
@@ -0,0 +1,18 @@
+import pprint
+import yara
+
+YARA_RULE_DIR = "/yaramaster/yararules"
+rule_file_path = "{}/{}".format(YARA_RULE_DIR, "02_strings_and_condition.yar")
+
+# Callback関数を定義
+def scan_callback(data):
+    print(type(data))
+    pprint.pprint(data)
+    
+    return yara.CALLBACK_CONTINUE
+
+# YARAルールのコンパイル
+rules = yara.compile(filepath=rule_file_path)
+
+# Callback関数を指定してYARAスキャン
+matches = rules.match(filepath="/yaramaster/test.txt", callback=scan_callback, which_callbacks=yara.CALLBACK_MATCHES)
